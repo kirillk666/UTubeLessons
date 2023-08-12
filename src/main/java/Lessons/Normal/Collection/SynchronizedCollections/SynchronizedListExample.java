304f771a30b238a712b6c63093ede24764fb84dd
kirillk <kirillll1994@mail.ru>
2023-08-12 13:40:50 +0300

    введение в многопоточность

--- src/main/java/Lessons/Normal/Collection/SynchronizedCollections/SynchronizedListExample.java
+++ src/main/java/Lessons/Normal/Collection/SynchronizedCollections/SynchronizedListExample.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class Example1 {
+public class SynchronizedListExample {
 
     public static void main(String[] args) throws InterruptedException {
 
