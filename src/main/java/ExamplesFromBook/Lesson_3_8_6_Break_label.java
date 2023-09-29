35c98f48942ee13f0c8aa2938ad42b4a8af8ced0
kirillk <kirillll1994@mail.ru>
2023-09-29 18:55:24 +0300

    бредовые примеры из книги и головы

---
+++ src/main/java/ExamplesFromBook/Lesson_3_8_6_Break_label.java
@@ -0,0 +1,24 @@
+package ExamplesFromBook;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Lesson_3_8_6_Break_label {
+    public static void main(String[] args) {
+
+        int count = 0;
+
+        System.out.println("Надо отработать 666 часов");
+        checkNumbers:
+        while (count < 666) {
+            for(int i = 0; i<8; i++) { //break label нужен, чтобы прервать вложенный цикл, хз какой придумать
+                count += 1;
+                if(count == 666){
+                   System.out.println("Молодец, отработал 666 часов!");
+                   break checkNumbers;
+               }
+            }
+            System.out.println("Осталось отработать " + (666 - count));
+        }
+    }
+}
